b7d7bf4b479f2f1d8168e83ab2d3a521a58e6138
JVSMOTA <jvmota.sb@gmail.com>
2024-08-25 14:51:15 -0300

    🐛 fix: corrigir condições para funcionamento de tipos de commit com escopo

--- .commit-icons.sh
+++ .commit-icons.sh
@@ -2,29 +2,29 @@
 
 commit_message=$1
 
-if [ "${commit_message#feat:}" != "$commit_message" ]; then
+if [ "${commit_message#feat}" != "$commit_message" ]; then
     emoji="✨"
-elif [ "${commit_message#fix:}" != "$commit_message" ]; then
+elif [ "${commit_message#fix}" != "$commit_message" ]; then
     emoji="🐛"
-elif [ "${commit_message#chore:}" != "$commit_message" ]; then
+elif [ "${commit_message#chore}" != "$commit_message" ]; then
     emoji="🔧"
-elif [ "${commit_message#docs:}" != "$commit_message" ]; then
+elif [ "${commit_message#docs}" != "$commit_message" ]; then
     emoji="📝"
-elif [ "${commit_message#style:}" != "$commit_message" ]; then
+elif [ "${commit_message#style}" != "$commit_message" ]; then
     emoji="💄"
-elif [ "${commit_message#refactor:}" != "$commit_message" ]; then
+elif [ "${commit_message#refactor}" != "$commit_message" ]; then
     emoji="♻️"
-elif [ "${commit_message#test:}" != "$commit_message" ]; then
+elif [ "${commit_message#test}" != "$commit_message" ]; then
     emoji="✅"
-elif [ "${commit_message#perf:}" != "$commit_message" ]; then
+elif [ "${commit_message#perf}" != "$commit_message" ]; then
     emoji="🚀"
-elif [ "${commit_message#ci:}" != "$commit_message" ]; then
+elif [ "${commit_message#ci}" != "$commit_message" ]; then
     emoji="👷"
-elif [ "${commit_message#build:}" != "$commit_message" ]; then
+elif [ "${commit_message#build}" != "$commit_message" ]; then
     emoji="🏗️"
-elif [ "${commit_message#revert:}" != "$commit_message" ]; then
+elif [ "${commit_message#revert}" != "$commit_message" ]; then
     emoji="⏪"
-elif [ "${commit_message#init:}" != "$commit_message" ]; then
+elif [ "${commit_message#init}" != "$commit_message" ]; then
     emoji="🎉"
 else
     emoji=""
